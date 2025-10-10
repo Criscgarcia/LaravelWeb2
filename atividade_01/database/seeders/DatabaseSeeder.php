aa2c90ad01a2bf21543ba31c6153c53489d6f550
Criswxyz <garciacaldas7@gmail.com>
2025-10-09 22:16:47 -0300

    Inclusão de Relacionamento N para N Empréstimo

--- atividade_01/database/seeders/DatabaseSeeder.php
+++ atividade_01/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             AuthorPublisherBookSeeder::class,
+            UserBorrowingSeeder::class,
         ]);
     }
 }
